e6a4dab6920f14256b6660aa322b31adb0e67b6a
vladyslav1910 <vladyslavbez13@gmail.com>
2024-08-14 14:41:13 +0300

    Covered autotests

---
+++ cypress/support/selectors/checkBox.js
@@ -0,0 +1,8 @@
+const checkBox = {
+    titleCheckBox: '[class="text-center"]',
+    homeCheckbox: 'input[type="checkbox"]',
+    expandAllBtn: '[title="Expand all"]',
+    collapseAllBtn: '[title="Collapse all"]',
+    permanentAddressInput: '[id="permanentAddress"]',
+    clickOnCheckbox: '[class="rct-checkbox"]'
+  };
